97769db4250ac96fb53270a8da2540390e2a6801
Geattt <kent.ma04@gmail.com>
2024-10-31 13:22:17 +0800

    Modified greeting message on master

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello from master branch!" << std::endl;
+    std::cout << "Hello from master with a different message!" << std::endl;
     return 0;
 }
